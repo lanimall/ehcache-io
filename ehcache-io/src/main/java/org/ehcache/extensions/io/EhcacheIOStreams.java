f0af69f0ee6d00e68a6233b00333f1b1eaed0d06
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2018-08-10 08:17:22 -0400

    added new getStreamEntryKeys method

--- ehcache-io/src/main/java/org/ehcache/extensions/io/EhcacheIOStreams.java
+++ ehcache-io/src/main/java/org/ehcache/extensions/io/EhcacheIOStreams.java
@@ -1,12 +1,14 @@
 package org.ehcache.extensions.io;
 
 import net.sf.ehcache.Cache;
+import net.sf.ehcache.Ehcache;
 import org.ehcache.extensions.io.impl.EhcacheInputStream;
 import org.ehcache.extensions.io.impl.EhcacheOutputStream;
 import org.ehcache.extensions.io.impl.EhcacheStreamUtils;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
 /**
  * Created by fabien.sanglier on 7/27/18.
@@ -23,6 +25,7 @@ public class EhcacheIOStreams {
     private static int DEFAULT_INPUTSTREAM_BUFFER_SIZE = 512 * 1024; // 512kb
     private static final long DEFAULT_OPEN_TIMEOUT = 10000;
     private static boolean DEFAULT_ALLOW_NULL_STREAM = false;
+    private static boolean DEFAULT_GETALLKEYS_EXPIRATION_CHECK = false;
 
     private static final Integer inputStreamBufferSize = getPropertyAsInt(PROP_INPUTSTREAM_BUFFERSIZE, DEFAULT_INPUTSTREAM_BUFFER_SIZE);
     private static final Integer outputStreamBufferSize = getPropertyAsInt(PROP_OUTPUTSTREAM_BUFFERSIZE, DEFAULT_OUTPUTSTREAM_BUFFER_SIZE);
@@ -60,67 +63,83 @@ public class EhcacheIOStreams {
     //////////////////////////// Public Utils
 
     /**
-     * Creates a new buffered output stream to write data to a cache
-     * with the specified buffer size.
+     * Check if a Stream entry exist in cache
      *
-     * @param       cache           the underlying cache to access
-     * @param       cacheKey        the underlying cache key to read data from
-     * @return      true if entry exists
-     * @exception   EhcacheStreamException if cache is null, disabled, or cacheKey is null.
+     * @param      cache  the backend cache
+     * @param      cacheKey  the public cache key for this stream entry
+     * @return     true if Stream entry is in cache
+     * @exception  EhcacheStreamException if cache or cacheKey are not valid
      */
-    public static boolean checkStreamEntryExist(Cache cache, Object cacheKey) throws EhcacheStreamException {
+    public static boolean containStreamEntry(Ehcache cache, Object cacheKey) throws EhcacheStreamException {
         checkValid(cache, cacheKey);
 
-        return null != new EhcacheStreamUtils(cache,cacheKey).getStreamMasterFromCache();
+        return new EhcacheStreamUtils(cache).containsStreamEntry(cacheKey);
     }
 
     /**
-     * Creates a new buffered output stream to write data to a cache
-     * with the specified buffer size.
+     * Get a list of all the public keys (the key objects used by the client apps) in cache
+     *
+     * @return      List of public key objects
+     */
+
+    /**
+     * Get list of all the cache entry keys
+     *
+     * @param      cache  the backend cache
+     * @return     List of stream entry keys in cache
+     * @exception  EhcacheStreamException if cache or cacheKey are not valid
+     */
+    public static List getStreamEntryKeys(Ehcache cache) throws EhcacheStreamException {
+        checkValid(cache);
+
+        return new EhcacheStreamUtils(cache).getAllStreamEntryKeys(DEFAULT_GETALLKEYS_EXPIRATION_CHECK);
+    }
+
+    /**
+     * Remove a stream entry from cache
      *
      * @param       cache           the underlying cache to access
      * @param       cacheKey        the underlying cache key to read data from
      * @return      true if the value was removed
      * @exception   EhcacheStreamException if cache is null, disabled, or cacheKey is null, OR if the remove operation was not successful
      */
-    public static boolean removeStreamEntry(Cache cache, Object cacheKey) throws EhcacheStreamException {
+    public static boolean removeStreamEntry(Ehcache cache, Object cacheKey) throws EhcacheStreamException {
         return removeStreamEntry(cache, cacheKey, streamOpenTimeouts);
     }
 
     /**
-     * Creates a new buffered output stream to write data to a cache
-     * with the specified buffer size.
+     * Remove a stream entry from cache
      *
      * @param       cache           the underlying cache to access
      * @param       cacheKey        the underlying cache key to read data from
-     * @param       openTimeout     the timeout for the stream reader open
+     * @param       openTimeout     the timeout on the stream open operation
      * @return      true if the value was removed
      * @exception   EhcacheStreamException if cache is null, disabled, or cacheKey is null, OR if the remove operation was not successful
      */
-    public static boolean removeStreamEntry(Cache cache, Object cacheKey, long openTimeout) throws EhcacheStreamException {
+    public static boolean removeStreamEntry(Ehcache cache, Object cacheKey, long openTimeout) throws EhcacheStreamException {
         checkValid(cache, cacheKey);
 
-        return new EhcacheStreamUtils(cache,cacheKey).removeEhcacheStreamEntry(openTimeout);
+        return new EhcacheStreamUtils(cache).removeStreamEntry(cacheKey, openTimeout);
     }
 
     //////////////////////////// InputStream
 
-    public static InputStream getInputStream(Cache cache, Object cacheKey) throws EhcacheStreamException {
+    public static InputStream getInputStream(Ehcache cache, Object cacheKey) throws EhcacheStreamException {
         return getInputStream(cache, cacheKey, streamAllowNulls);
     }
 
-    public static InputStream getInputStream(Cache cache, Object cacheKey, boolean allowNullStream) throws EhcacheStreamException {
+    public static InputStream getInputStream(Ehcache cache, Object cacheKey, boolean allowNullStream) throws EhcacheStreamException {
         return getInputStream(cache, cacheKey, allowNullStream, inputStreamBufferSize);
     }
 
-    public static InputStream getInputStream(Cache cache, Object cacheKey, boolean allowNullStream, int bufferSize) throws EhcacheStreamException {
+    public static InputStream getInputStream(Ehcache cache, Object cacheKey, boolean allowNullStream, int bufferSize) throws EhcacheStreamException {
         return getInputStream(cache, cacheKey, allowNullStream, bufferSize, streamOpenTimeouts);
     }
 
-    public static InputStream getInputStream(Cache cache, Object cacheKey, boolean allowNullStream, int bufferSize, long openTimeout) throws EhcacheStreamException {
+    public static InputStream getInputStream(Ehcache cache, Object cacheKey, boolean allowNullStream, int bufferSize, long openTimeout) throws EhcacheStreamException {
         checkValid(cache, cacheKey);
 
-        if(!allowNullStream || allowNullStream && checkStreamEntryExist(cache,cacheKey)){
+        if(!allowNullStream || allowNullStream && containStreamEntry(cache, cacheKey)){
             return new EhcacheInputStream(
                     cache,
                     cacheKey,
@@ -133,19 +152,19 @@ public class EhcacheIOStreams {
 
     //////////////////////////// OutputStream
 
-    public static OutputStream getOutputStream(Cache cache, Object cacheKey) throws EhcacheStreamException {
+    public static OutputStream getOutputStream(Ehcache cache, Object cacheKey) throws EhcacheStreamException {
         return getOutputStream(cache, cacheKey, outputStreamDefaultOverride);
     }
 
-    public static OutputStream getOutputStream(Cache cache, Object cacheKey, boolean override) throws EhcacheStreamException {
+    public static OutputStream getOutputStream(Ehcache cache, Object cacheKey, boolean override) throws EhcacheStreamException {
         return getOutputStream(cache, cacheKey, override, outputStreamBufferSize);
     }
 
-    public static OutputStream getOutputStream(Cache cache, Object cacheKey, boolean override, int bufferSize) throws EhcacheStreamException {
+    public static OutputStream getOutputStream(Ehcache cache, Object cacheKey, boolean override, int bufferSize) throws EhcacheStreamException {
         return getOutputStream(cache, cacheKey, override, bufferSize, streamOpenTimeouts);
     }
 
-    public static OutputStream getOutputStream(Cache cache, Object cacheKey, boolean override, int bufferSize, long openTimeout) throws EhcacheStreamException {
+    public static OutputStream getOutputStream(Ehcache cache, Object cacheKey, boolean override, int bufferSize, long openTimeout) throws EhcacheStreamException {
         checkValid(cache, cacheKey);
 
         EhcacheOutputStream ehcacheStream = new EhcacheOutputStream(
@@ -161,12 +180,17 @@ public class EhcacheIOStreams {
 
     //////////////////////////// Internal Validators
 
-    private static void checkValid(Cache cache, Object cacheKey) throws EhcacheStreamException {
+    private static void checkValid(Ehcache cache) throws EhcacheStreamException {
         if(cache == null)
             throw new EhcacheStreamException("Cache may not be null");
 
         if(cache.isDisabled())
             throw new EhcacheStreamException("Cache is disabled");
+
+    }
+
+    private static void checkValid(Ehcache cache, Object cacheKey) throws EhcacheStreamException {
+        checkValid(cache);
 
         if(cacheKey == null)
             throw new EhcacheStreamException("cacheKey may not be null");
