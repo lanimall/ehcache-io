cfcb226a86021f6378da7bd479a6fbcc803017f0
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2018-09-13 10:00:25 -0400

    making all the constants final

--- ehcache-io/src/main/java/org/ehcache/extensions/io/impl/EhcacheStreamUtils.java
+++ ehcache-io/src/main/java/org/ehcache/extensions/io/impl/EhcacheStreamUtils.java
@@ -15,22 +15,22 @@ import java.util.List;
  * Created by fabien.sanglier on 8/2/18.
  */
 public class EhcacheStreamUtils {
-    public static String PROP_INPUTSTREAM_BUFFERSIZE = "ehcache.extension.io.inputstream.buffersize";
-    public static String PROP_INPUTSTREAM_OPEN_TIMEOUTS = "ehcache.extension.io.inputstream.opentimeout";
-    public static String PROP_INPUTSTREAM_ALLOW_NULLSTREAM = "ehcache.extension.io.inputstream.allownull";
+    public static final String PROP_INPUTSTREAM_BUFFERSIZE = "ehcache.extension.io.inputstream.buffersize";
+    public static final String PROP_INPUTSTREAM_OPEN_TIMEOUTS = "ehcache.extension.io.inputstream.opentimeout";
+    public static final String PROP_INPUTSTREAM_ALLOW_NULLSTREAM = "ehcache.extension.io.inputstream.allownull";
 
-    public static String PROP_OUTPUTSTREAM_BUFFERSIZE = "ehcache.extension.io.outputstream.buffersize";
-    public static String PROP_OUTPUTSTREAM_OVERRIDE = "ehcache.extension.io.outputstream.override";
-    public static String PROP_OUTPUTSTREAM_OPEN_TIMEOUTS = "ehcache.extension.io.outputstream.opentimeout";
+    public static final String PROP_OUTPUTSTREAM_BUFFERSIZE = "ehcache.extension.io.outputstream.buffersize";
+    public static final String PROP_OUTPUTSTREAM_OVERRIDE = "ehcache.extension.io.outputstream.override";
+    public static final String PROP_OUTPUTSTREAM_OPEN_TIMEOUTS = "ehcache.extension.io.outputstream.opentimeout";
 
 
-    public static int DEFAULT_OUTPUTSTREAM_BUFFER_SIZE = 128 * 1024; // 128kb
-    public static boolean DEFAULT_OUTPUTSTREAM_OVERRIDE = true;
-    public static int DEFAULT_INPUTSTREAM_BUFFER_SIZE = 512 * 1024; // 512kb
+    public static final int DEFAULT_OUTPUTSTREAM_BUFFER_SIZE = 128 * 1024; // 128kb
+    public static final boolean DEFAULT_OUTPUTSTREAM_OVERRIDE = true;
+    public static final int DEFAULT_INPUTSTREAM_BUFFER_SIZE = 512 * 1024; // 512kb
     public static final long DEFAULT_OUTPUTSTREAM_OPEN_TIMEOUT = 10000L;
     public static final long DEFAULT_INPUTSTREAM_OPEN_TIMEOUT = 2000L;
-    public static boolean DEFAULT_INPUTSTREAM_ALLOW_NULLSTREAM = false;
-    public static boolean DEFAULT_RELEASELOCK_CHECKTHREAD_OWNERSHIP = true;
+    public static final boolean DEFAULT_INPUTSTREAM_ALLOW_NULLSTREAM = false;
+    public static final boolean DEFAULT_RELEASELOCK_CHECKTHREAD_OWNERSHIP = true;
 
     /*
      * The Internal Ehcache cache object
