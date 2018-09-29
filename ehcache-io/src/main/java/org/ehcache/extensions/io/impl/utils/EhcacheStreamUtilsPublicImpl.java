dda585042bc01e6624cf5cc8882d4b490792dbd1
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2018-09-29 11:36:38 -0400

    moved code for getAllStreamEntryKeys into the internal util, where it should be

--- ehcache-io/src/main/java/org/ehcache/extensions/io/impl/utils/EhcacheStreamUtilsPublicImpl.java
+++ ehcache-io/src/main/java/org/ehcache/extensions/io/impl/utils/EhcacheStreamUtilsPublicImpl.java
@@ -2,14 +2,9 @@ package org.ehcache.extensions.io.impl.utils;
 
 import net.sf.ehcache.Ehcache;
 import org.ehcache.extensions.io.EhcacheStreamException;
-import org.ehcache.extensions.io.impl.model.EhcacheStreamKey;
-import org.ehcache.extensions.io.impl.model.EhcacheStreamMaster;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -23,7 +18,9 @@ public class EhcacheStreamUtilsPublicImpl implements IEhcacheStreamUtils {
     */
     final EhcacheStreamUtilsInternal ehcacheStreamUtilsInternal;
 
-    public EhcacheStreamUtilsPublicImpl(Ehcache cache) {
+    final PropertyUtils.ConcurrencyMode concurrencyMode = PropertyUtils.getEhcacheIOStreamsConcurrencyMode();
+
+    EhcacheStreamUtilsPublicImpl(Ehcache cache) {
         //TODO: we should check if the cache is not null but maybe enforce "Pinning"?? (because otherwise cache chunks can disappear and that would mess up the data consistency...)
         this.ehcacheStreamUtilsInternal = new EhcacheStreamUtilsInternal(cache);
     }
@@ -44,10 +41,6 @@ public class EhcacheStreamUtilsPublicImpl implements IEhcacheStreamUtils {
     @Override
     public boolean removeStreamEntry(final Object cacheKey, long timeout) throws EhcacheStreamException {
         boolean removed = false;
-
-        PropertyUtils.ConcurrencyMode concurrencyMode = PropertyUtils.getEhcacheIOStreamsConcurrencyMode();
-        if(logger.isDebugEnabled())
-            logger.debug("Creating a stream reader with Concurrency mode: {}", concurrencyMode.getPropValue());
 
         switch (concurrencyMode){
             case WRITE_PRIORITY:
@@ -84,60 +77,27 @@ public class EhcacheStreamUtilsPublicImpl implements IEhcacheStreamUtils {
      * Get a list of all the public keys (the key objects used by the client apps) in cache
      *
      * @param       checkForExpiry        if true, returns only the keys that are not expired. NOTE: this could take much longer time if cache is large.
-     * @return      A list of public key objects
+     * @return      A list of all the public key objects
      */
     @Override
     public List getAllStreamEntryKeys(boolean checkForExpiry){
-        List publicKeys;
-        List internalKeys = (checkForExpiry)?ehcacheStreamUtilsInternal.getCache().getKeysWithExpiryCheck():ehcacheStreamUtilsInternal.getCache().getKeys();
-
-        if(null != internalKeys && internalKeys.size() > 0) {
-            publicKeys = new ArrayList(internalKeys.size());
-            Iterator it = internalKeys.iterator();
-            while( it.hasNext() ) {
-                Object cacheKey = it.next();
-                if(null != cacheKey
-                        && cacheKey.getClass().equals(EhcacheStreamKey.class)
-                        && ((EhcacheStreamKey)cacheKey).getChunkIndex() == EhcacheStreamKey.MASTER_INDEX){
-                    publicKeys.add(((EhcacheStreamKey)cacheKey).getCacheKey());
-                }
-            }
-        } else {
-            publicKeys = Collections.emptyList();
-        }
-
-        return publicKeys;
+        return ehcacheStreamUtilsInternal.getAllStreamMasterPublicKeys(checkForExpiry);
     }
 
+    /**
+     * Get a list of all the public keys (the key objects used by the client apps) in cache
+     *
+     * @param       checkForExpiry        if true, returns only the keys that are not expired. NOTE: this could take much longer time if cache is large.
+     * @param       includeNoReads        if true, includes only the keys that have no current read
+     * @param       includeNoWrites        if true, includes only the keys that have no current write
+     * @param       includeReadsOnly        if true, includes only the keys that have current reads
+     * @param       includeWritesOnly        if true, includes only the keys that have current writes
+
+     * @return      A list of public key objects
+     */
     @Override
-    public List getAllStreamEntryKeysFilteredByState(boolean checkForExpiry, boolean includeCurrentWrites, boolean includeCurrentReads){
-        List publicKeys;
-        List internalKeys = (checkForExpiry)?ehcacheStreamUtilsInternal.getCache().getKeysWithExpiryCheck():ehcacheStreamUtilsInternal.getCache().getKeys();
-
-        if(null != internalKeys && internalKeys.size() > 0) {
-            publicKeys = new ArrayList(internalKeys.size());
-            Iterator it = internalKeys.iterator();
-            while( it.hasNext() ) {
-                Object cacheKey = it.next();
-                if(null != cacheKey
-                        && cacheKey.getClass().equals(EhcacheStreamKey.class)
-                        && ((EhcacheStreamKey)cacheKey).getChunkIndex() == EhcacheStreamKey.MASTER_INDEX){
-
-                    EhcacheStreamMaster ehcacheStreamMaster = ehcacheStreamUtilsInternal.getStreamMasterFromCache(((EhcacheStreamKey)cacheKey).getCacheKey());
-                    if(includeCurrentWrites && ehcacheStreamMaster.getWriters()>0) {
-                        publicKeys.add(((EhcacheStreamKey) cacheKey).getCacheKey());
-                    }
-
-                    if(includeCurrentReads && ehcacheStreamMaster.getReaders()>0) {
-                        publicKeys.add(((EhcacheStreamKey) cacheKey).getCacheKey());
-                    }
-                }
-            }
-        } else {
-            publicKeys = Collections.emptyList();
-        }
-
-        return publicKeys;
+    public List getAllStreamEntryKeys(boolean checkForExpiry, boolean includeNoReads, boolean includeNoWrites, boolean includeReadsOnly, boolean includeWritesOnly){
+        return ehcacheStreamUtilsInternal.getAllStreamMasterPublicKeys(checkForExpiry, includeNoReads, includeNoWrites, includeReadsOnly, includeWritesOnly);
     }
 
     /////////////////////////////////
