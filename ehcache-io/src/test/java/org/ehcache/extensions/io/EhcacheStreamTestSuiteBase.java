d35e8e04eb29843c02a38a999a1b743aa54a8294
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2018-10-25 23:03:17 -0400

    adding the new tests

--- ehcache-io/src/test/java/org/ehcache/extensions/io/EhcacheStreamTestSuiteBase.java
+++ ehcache-io/src/test/java/org/ehcache/extensions/io/EhcacheStreamTestSuiteBase.java
@@ -1,9 +1,11 @@
 package org.ehcache.extensions.io;
 
 import org.ehcache.extensions.io.impl.*;
+import org.ehcache.extensions.io.impl.model.EhcacheStreamMasterTest;
 import org.ehcache.extensions.io.impl.readers.EhcacheInputStreamTest;
 import org.ehcache.extensions.io.impl.utils.EhcacheStreamCasTest;
 import org.ehcache.extensions.io.impl.utils.EhcacheStreamUtilsTest;
+import org.ehcache.extensions.io.impl.utils.cas.WaitTest;
 import org.ehcache.extensions.io.impl.writers.EhcacheOutputStreamTest;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -16,10 +18,12 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        EhcacheStreamMasterTest.class,
+        WaitTest.class,
+        EhcacheStreamUtilsTest.class,
         EhcacheStreamCasTest.class,
         EhcacheInputStreamTest.class,
         EhcacheOutputStreamTest.class,
-        EhcacheStreamUtilsTest.class,
         EhcacheStreamConcurrentTest.class
 })
 public class EhcacheStreamTestSuiteBase {
